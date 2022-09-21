d8922be50988593022b8c5c61cbf3fc8ebd8807c
dude34561 <100492483+dude34561@users.noreply.github.com>
2022-09-20 20:11:18 -0400

    Add files via upload

---
+++ h.js
@@ -0,0 +1,31 @@
+
+        console.log("Hi!!! I am on console!")
+        var fn="Siddharth"
+        var ln="Sabarish"
+        console.log(fn+" "+ln)
+        var fulln=fn.concat(ln)
+        console.log(fulln)
+        var lengthn=fulln.length
+        console.log(lengthn)
+        var wfulln=fulln.replace("Sabarish","Pillai")
+        console.log(wfulln)
+        var capitaln="SIDDHARTH"
+        var lowercase_firstname= capitaln.toLowerCase();
+        console.log(lowercase_firstname)
+        var lowercasen="siddharth"
+        var capital_firstname= lowercasen.toUpperCase();
+        console.log(capital_firstname)
+        var sqrroot= Math.sqrt(144)
+        console.log(sqrroot)
+        var max=Math.max(15,144,19478237,7047175,2134907091735,12735490172498,1973245981720)
+        console.log(max)
+        var min=Math.min(293,954,845,846,847,732)
+        console.log(min)
+        var power=Math.pow(4,4)
+        console.log(power)
+   var a=3
+   var b=3
+   console.log(a*b)
+   console.log(a/b)
+   console.log(a+b)
+   console.log(a-b)
